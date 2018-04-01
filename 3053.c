995d9456aa299c1795f0bf4fa098983f0f0aaa15
Plit00 <eovyxla14@gmail.com>
2018-04-01 22:13:42 +0900

    Create 3053.c

---
+++ 3053.c
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   cin >> x;
+
+	cout << fixed;
+	cout.precision(6);
+
+	cout << x*x*M_PI << endl;
+	cout << x*x * 2.0 << endl;
+}
